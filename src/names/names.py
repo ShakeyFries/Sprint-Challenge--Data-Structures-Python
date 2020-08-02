10f96f4c4aa5799e357be341ac985d066a3368f5
Chris Daley <Daleys@Chriss-iMac.local>
2020-08-02 17:22:46 -0600

    got my run time below half a second

--- src/names/names.py
+++ src/names/names.py
@@ -1,5 +1,5 @@
 import time
-from src.bst import BSTNode
+
 
 start_time = time.time()
 
@@ -14,10 +14,10 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 # first run time
 # runtime: 12.615012168884277 seconds
@@ -25,6 +25,11 @@ for name_1 in names_1:
 # importing binary search tree as I remember being told it was a faster way to search.
 
 # implement bst somehow?
+import sys
+sys.path.append('src/bst.py')
+from bst import BSTNode
+
+
 bst = BSTNode(names_1[0])
 
 # insert names_into binary search tree
@@ -40,7 +45,10 @@ end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
+# Improved run time!
+# runtime: 0.23639607429504395 seconds
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  There are are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
